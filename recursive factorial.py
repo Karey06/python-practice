f161748f000528113c074a183c607503061ac6ab
Karey06 <joyphenomenal@gmail.com>
2025-04-26 20:39:44 +0300

    I did recursive factorial

---
+++ recursive factorial.py
@@ -0,0 +1,15 @@
+def factorial(number):
+    if number == 0 or number == 1:
+        return 1
+    else:
+        return number*factorial(number-1)
+while True:
+    try:
+        number = int(input("Input number:"))
+        if number <0:
+            print("Invalid input")
+        else:
+            print(factorial(number))
+            break
+    except ValueError:
+        print("Invalid input")
