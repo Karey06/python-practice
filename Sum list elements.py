dc30e37f7340454c07d93bb0ce893b93664a04aa
Karey06 <joyphenomenal@gmail.com>
2025-04-26 19:27:12 +0300

    I found the sum of elements in a list

--- Sum list elements.py
+++ Sum list elements.py
@@ -1,1 +1,7 @@
-#thankyou
+#the sum of elements in a list
+#Define the elements in the list and assign it a variable
+the_list =[1,2,3,4,5,6]
+#find the sum of elements in the list
+sum_of_list =sum(the_list)
+#print the sum of the elements
+print(sum_of_list)
